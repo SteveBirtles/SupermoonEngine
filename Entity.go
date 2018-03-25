c65c02f059daf8dc04691c033884431cdb16c506
Steve Birtles <sbirtles@farnborough.ac.uk>
2018-03-25 22:15:55 +0100

    Some transit fixing needed.

--- Entity.go
+++ Entity.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"time"
+	"math/rand"
+	"fmt"
+	"math"
+)
+
 type Entity struct {
 
   id uint32
@@ -29,10 +36,60 @@ type Entity struct {
 
 func updateEntities() {
 
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+
 	for i := range entities {
 
-		entities[i].x += entities[i].dx / 60
-		entities[i].y += entities[i].dy / 60
+		intX := math.Floor(entities[i].x)
+		inTransitX := entities[i].x != intX
+		intNextX := math.Floor(entities[i].x + entities[i].dx / 60)
+
+		fmt.Printf("x:%f ix:%f dx:%f nx:%f\n", entities[i].x, intX, entities[i].dx, intNextX)
+
+		if intX != intNextX {
+			entities[i].x = intNextX
+			inTransitX = false
+		} else {
+			entities[i].x += entities[i].dx / 60
+		}
+
+		intY := math.Floor(entities[i].y)
+		inTransitY := entities[i].y != intY
+		intNextY := math.Floor(entities[i].y + entities[i].dy / 60)
+
+		if intY != intNextY {
+			entities[i].y = intNextY
+			inTransitY = false
+		} else {
+			entities[i].y += entities[i].dy / 60
+		}
+
+		if !(inTransitX || inTransitY) {
+
+			dx := 0.0
+			dy := 0.0
+
+			d := r.Intn(4)
+
+			fmt.Printf("%d\n", d)
+
+			switch d {
+			case 0:
+				dy = -1
+			case 1:
+				dx = -1
+			case 2:
+				dy = 1
+			case 3:
+				dx = 1
+			}
+
+			entities[i].dx = dx
+			entities[i].dy = dy
+
+		}
+
 
 	}
 
@@ -76,19 +133,25 @@ func updateEntities() {
 
 	for _, e := range entities {
 
+		inTransit := e.y != float64(int(e.y))
+
 		switch viewDirection {
 		case 0:
 			x = int(e.x)
-			y = int(e.y)+1
+			y = int(e.y)
+			if inTransit { y++ }
 		case 1:
-			x = -int(e.y)-1
+			x = -int(e.y)
 			y = int(e.x)
+			if inTransit { x-- }
 		case 2:
 			x = -int(e.x)
-			y = -int(e.y)-1
+			y = -int(e.y)
+			if inTransit { y-- }
 		case 3:
-			x = int(e.y)+1
+			x = int(e.y)
 			y = -int(e.x)
+			if inTransit { x++ }
 		}
 
 		if x < iMin || x > iMax || j < jMin	|| j > jMax { continue }
